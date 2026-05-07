455b7da483fc455811ac08a3dc90b17e76737c43
yul007 <yulianahguerra@gmail.com>
2026-05-07 11:46:46 -0500

    Esta es la estructura basica parece

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth Scrolling for Navigation
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
